6f7266b27f71746f416b2fec1c7e595585e53e96
iii <iii@userteki-MacBook.local>
2017-06-19 10:10:38 +0800

    v2

---
+++ BradS11/BradAPI.swift
@@ -0,0 +1,31 @@
+//
+//  BradAPI.swift
+//  BradS11
+//
+//  Created by iii on 2017/6/19.
+//  Copyright © 2017年 iii. All rights reserved.
+//
+
+import Foundation
+
+class Brad01 {
+    var x = 1
+    func m1() {
+        print("Brad01:m1():x = \(x)")
+    }
+}
+class Rect {
+    var x:Int = 0
+    var y:Int = 0
+    var w:Int = 0
+    var h:Int = 0
+}
+
+struct Brad02 {
+    var x = 1
+    func m1() {
+        print("Brad01:m1():x = \(x)")
+    }
+}
+
+
